39225026749b4f5a7c80f11d3e3eca6cf15f1fc1
ahsanul0185 <ahsanul0185@gmail.com>
2025-01-30 05:01:30 +0600

    fixed page not found issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ import { Link, NavLink } from "react-router-dom";
 const navItems = [
   {
     name: "Dashboard",
-    link: "/",
+    link: "",
   },
   {
     name: "Order List",
-    link: "/orders",
+    link: "orders",
   },
   {
     name: "Address List",
-    link: "/address-list",
+    link: "address-list",
   },
 ];
 
